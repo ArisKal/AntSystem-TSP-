85c7f5dfc9091a04168dc23cd2d15199567b9cb3
ArisKal <aris.kalantzis95@gmail.com>
2020-12-06 13:32:03 +0200

    Add gitignore file

---
+++ AntSystem/ReadData.py
@@ -0,0 +1,119 @@
+import xml.etree.ElementTree as ET
+import numpy as np
+
+
+class ReadData:
+    """
+    Κλάση ReadData η οποία θα διβάζει τα προβλήματα (TSP) σε μορφή *.xml και τα μετρέπει σε πίνακα (n*n) n = διάσταση
+    προβλήματος.
+    Ta προβλήματα μπρούν να κατέβουν απο τον ιστότοπο:
+    http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/XML-TSPLIB/instances/.
+    ΠΛηροφορίες για τη δομή του xml στον ιστότοπο:
+    http://comopt.ifi.uni-heidelberg.de/software/TSPLIB95/XML-TSPLIB/Description.pdf
+
+    """
+
+    def __init__(self, name_of_file):
+        """
+        Ορίζει το όνομα του αρχείου *.xml
+        :param name_of_file: το όνομα του αρχείου.
+        """
+        self.name_of_file = name_of_file
+
+    def set_name_of_file(self, name_of_file):
+        """
+        Ορίζει το όνομα του αρχείου *.xml
+        :param name_of_file: το όνομα του αρχείου.
+        """
+        self.name_of_file = name_of_file
+
+    def get_name_of_file(self):
+        """
+        Επιστροφή του ονόματος του αρχειου.
+        :return: Όνομα αρχείου
+        """
+        return self.name_of_file
+
+    @staticmethod
+    def read_file(name_of_file):
+        """
+        Διαβάσμα αρχείου *.xml και επιστροφή του δέτρου.
+        :param name_of_file:Το όνομα του αρχείου
+        :return:Επιστοφή δέντρου
+        """
+        try:
+            tree = ET.parse(name_of_file)
+        except FileNotFoundError:
+            print("Το αρχείο δεν υπάρχει")
+        return tree
+
+    @staticmethod
+    def get_dimesion(the_file):
+        """
+        Μέθοδος που επιστέφει την διάσταση του προβήματος,
+        Κάθε αρχείο .xml περιέχει το tag "vertex" όσα "vertex" έχει το αρχείο τόσοι έιναι και οι κομβοι άρα και η διάσταση
+        του προβλήματος είναι οι κομβοι.
+        :param the_file:Το όνομα του αρχείου.
+        :return:Επιστρέφει την διάσταση του προβηματος
+        """
+        count = 0
+        tree = the_file
+        root = tree.getroot()
+        # Για όλο το αρχείο όταν βρήκει την λέξη "vertex" αύξησε τον μετρητή κατά 1.
+        for vertex in root.iter('vertex'):
+            count += 1
+
+        return count
+    @staticmethod
+    def create_graph(the_file, dimesion):
+        """
+        Δημιουργία του γράφου σε αυτή την μέθοδο απο το αρχείο βρήσκουμε τα κόστη και να εισχωρούε σε ενα πίνακα d*d
+        οι διαγώνιες τιμές είναι 0
+        Απο το αρχείο xml απο το tag edge μπορούμε να πάρουμε τα κόστη.
+        :param the_file: το όνομα του αρχείου
+        :param dimesion: Διάσταση προβλήματος
+        :return:
+        """
+        tree = the_file
+        root = tree.getroot()
+        # Δημιουργία πίνακα dimesion * dimesion γεμάτος με 0
+        arr = np.zeros((dimesion, dimesion))
+        # Ορίζω τον δείκτη της γραμμής με 0
+        row = 0
+        # Ορίζω τον δείκτη της στήλης με 0
+        column = 0
+        # Για κάθε tag edge:
+        for edge in root.iter('edge'):
+            """
+            Όταν ο δείτης την γραμμής είναι ίδιος με τον δείκτη της στήλης
+            τοτε στον πίνακα γίνεται εισχώρηση του 0 και ο δείκτης της στήλης αυξανεται κατά 1
+            """
+            if row == column:
+                arr[row][column] = 0
+                column += 1
+            """
+            Όταν ο δείκτης της γραμμής και της στήλης δεν είναι όμοιοι τότε εισχωρείται το κόστος 
+            """
+            if row != column:
+                # Απο το tag 'vertex' πάρε το attribute 'cost'(έναι ένας πραγματικός αριθμος)
+                arr[row][column] = edge.get('cost')
+                column += 1
+                # Οταν ο δείκτης της στήλης φτάσει στο τέλος τοτε αλλάζουμς γραμμή αυξάνοντας κατά 1 και μηδέζεται η
+                # στήλη
+            if column == dimesion:
+                column = 0
+                row += 1
+            # if row == dimesion:
+            #     break
+        return arr
+
+
+# read_data = ReadData("data/gr17.xml")
+# # read_data.set_name_of_file("data/att48.xml")
+# name = read_data.get_name_of_file()
+#
+# file = ReadData.read_file(name)
+# dimesion = ReadData.get_dimesion(file)
+# print(dimesion)
+# graph = ReadData.create_graph(file, dimesion)
+# print(graph)
