b6d2ea292a3bab17e70d403389563da0a99c603e
ArisKal <aris.kalantzis95@gmail.com>
2020-12-14 01:50:42 +0200

    Add data,main,finish Colony.py,Reafactoring names

--- AntSystem/Ant.py
+++ AntSystem/Ant.py
@@ -1,113 +1,155 @@
 class Ant:
     """
     Κλάση Ant περιέχει όλα τα χαρακτηρηστικά του μηρμηγκιού
-    - self.ant_number: είναι ο μοναδικός χαρακτηριστικός αριθμος που έχεο το κάθε μηρμήγκι.
-    - self.starting_node: είναι ο κόμβος απο τον οποίο θα ξεκινησει το μυρμήγκι.
-    - self.located_nodes: έναι ο κόμβος που βρήσκεται κάθε στιγμή το μυρμήγκι.
-    - self.allowed_noded: είναι μία λίστα με τους κόμβους που επριτρέπεται να επισκεφτει το μηρμήγκι.
-    - self.tour: είναι μία λίστα με τους κόμβους που έχει επισκεφτει το μηρμήγκι.
+    - self.ant_id:To id που έχει το μυρμήγκι(integer).
+    - self.starting_town: Η αρχική πόλη απο τον οποία θα ξεκινήσει την διαδρομή το μυρμήγκι(integer).
+    - self.located_town: Εναι η πόλη που βρίσκεται το μυρμήγκι(integer).
+    - self.allowed_town: Είναι μια λίστα με τις πόλεις που επιτρέπεται το μυρμήγκι να επισκεφτεί(tabu list).
+    - self.tour: Είναι μια λίστα με τις πόλεις που έχει επισκεφτεί το μυρμήγκι(list).
 
-    :param ant_id o αριθμός του μηρμηγκιού.
-    :param starting_node o αρχικός κόμβος που τοποθετειται το μηρμήγκι,
+    :param ant_id: Το id του μυρμηγκιού (integer).
+    :param starting_town: Η αρχική πόλη (integer)
     """
 
     # Κατασκευαστής
-    def __init__(self, ant_id, starting_node):
+    def __init__(self, ant_id, starting_town):
         self.ant_id = ant_id
-        self.starting_node = starting_node
-        # Στον κατασκευαστή ο τρέχων κόμβος είναι και ο αρχικός κόμβος.
-        self.located_node = starting_node
-        # Κένή λίστα που θα περιέχει τους κόμβος που επριτρέπεται να επισκεφτει το μηρμήγκι.
-        self.allowed_nodes = list()
-        # Κενή λίστα με τους κόμβους που έχει επισκεφτει το μηρμήγκι.
+        self.starting_town = starting_town
+        # Στον κατασκευαστή η τρέχων πόλη είναι και η αρχική πόλη.
+        self.located_town = starting_town
+        # Κένή λίστα που θα περιέχει όλες τις πόλεις που επριτρέπεται να επισκεφτει το μηρμήγκι.
+        self.allowed_towns = list()
+        # Κενή λίστα με τις πόλεις που έχει επισκεφτει το μηρμήγκι.
         self.tour = list()
 
-    # Ορίζει ένας αριθμό στο μυρμήγκι.
     def set_ant_id(self, ant_id):
+        """
+        Ορίζει το id του μυρμηγκιού.
+        :param ant_id: integer
+        """
         self.ant_id = ant_id
 
-    # Ορίζει τον αρχικό κόμβο που θα τοποθετηθέι το μηρμήγκι.
-    def set_starting_node(self, starting_node):
-        self.starting_node = starting_node
+    def set_starting_town(self, starting_town):
+        """
+        Ορίζει την αρχική πόλη
+        :param starting_town: integer
+        """
+        self.starting_town = starting_town
 
-    # Ορίζει τον τρέχων κόμβο
-    def set_located_node(self, located_node):
-        self.located_node = located_node
+    def set_located_town(self, located_town):
+        """
+        Οριζεί την τρέχων πόλη.
+        :param located_town: integer
+        """
+        self.located_town = located_town
 
-    # Ορίζει την λίστα με τους επιτρεπτόμενους κόμβους.
-    def set_allowed_nodes(self, allowed_nodes):
-        self.allowed_nodes = allowed_nodes
+    def set_allowed_towns(self, allowed_towns):
+        """
+        Ορίζει τις πόλεις που επιτρέπεται το μυρμήγκι να επισκεφτεί.
+        :param allowed_towns: list
+        """
+        self.allowed_towns = allowed_towns
 
-    # Ορίζει την λίστα με τους κόμβους που έχει επισκευφτεί το μηρμήγκι.
-    def set_tour(self, tour):
-        self.tour.append(tour)
+    def set_tour(self, town):
+        """
+        Πρόσθέτει στην λίστα με την διαδρομή μια πόλη.
+        :param town: integer
+        """
+        self.tour.append(town)
 
-    # Επιστρέφει τον αριθμό απο το μυρμήγκι.
     def get_ant_id(self):
+        """
+        Επιστρέφει το id του μυρμηγκιού.
+        :return: integer
+        """
         return self.ant_id
 
-    # Επιστρέφει τον αρχικό κόμβο.
-    def get_starting_node(self):
-        return self.starting_node
+    def get_starting_town(self):
+        """
+        Επιστρέφει την αρχική πόλη
+        :return: integer
+        """
+        return self.starting_town
 
-    # Επιστρέφει τον τρέχων κόμβο.
-    def get_located_node(self):
-        return self.located_node
+    def get_located_town(self):
+        """
+        Επιστρέφει την τρέχον πόλη.
+        :return: integer
+        """
+        return self.located_town
 
-    def get_allowed_nodes(self):
+    def get_allowed_towns(self):
         """
         Επιστροφή λίστας με τους επιτρεπτόμενους κόμβους.
-        :return: Λίστα
+        :return: list
         """
-        return self.allowed_nodes
+        return self.allowed_towns
 
-    # Επιστρέφει την λίστα με τους κόμβος που έχει επισκεφτεί.
     def get_tour(self):
+        """
+        Επιστρέφει την λίστα με την διαδρομή που έχει ακολουθήσει το μυρμήγκι.
+        :return: list
+        """
         return self.tour
 
     @staticmethod
-    def initialize_allowed_nodes(dimension):
+    def initialize_allowed_towns(dimension):
         """
-        Αρχικοποίηση των κόμβων που επιτρέπεται να επισκεφτεί το κάθε μυρμήγκι.
-        :param dimension: Η διάσταση του προβλήματος(ακέραιος).
-        :return: 'Εναν πίνακα με τους κόμβους που επιτρέπεται να επισκεφτεί το μυρμήγκι.
+        Αρχικοποίηση των πόλεων που επιτρέπεται να επισκεφτεί το μυρμήγκι.
+        :param dimension: Η διάσταση του προβλήματος(integer).
+        :return: Μια λίστα με τους κόμβους που μπορέι να επισεφτεί το μυρμήγκι.
         """
-        allowed_nodes = []
-        for node in range(0, dimension):
-            allowed_nodes.append(node)
-        return allowed_nodes
+        allowed_towns = []
+        for town in range(0, dimension):
+            allowed_towns.append(town)
+        return allowed_towns
 
     @staticmethod
-    def transition_probability(located_node, next_node, alpha, beta, pheromone, visibility, allowed):
+    def transition_probability(located_town, next_town, alpha, beta, pheromone, visibility, allowed_towns):
         """
         Σε αυτη την μέθοδο υπολογίζεται η πιθανότητα να παει ένα μυρμήγκι απο την πόλη i στην πόλη j :
         p(i|j) = pheromone[i,j] ^ α  *  visibility[i,j] ^ β / Σ pheromone[i,allowed_towns] ^  *
-                visibility[i,allowed_nodes] jE allowed nodes
+                visibility[i,allowed_towns] jE allowed towns
         p(i|j) = 0
-        :param located_node:Ο τρέχον κόμβος ποου βρίσκεται το μυρμίγκι (ακέραιος).
-        :param next_node:Ο επόμενος κόμβος που μπόρει να επισκεφτεί (ακέραιος).
+        :param located_towns:H τρέχον πόλη ποου βρίσκεται το μυρμίγκι (ακέραιος).
+        :param next_town:Η επόμη πόλη που μπόρει να επισκεφτεί (ακέραιος).
         :param alpha:Μια παράμετρος πραγματικός αριθμός.
         :param beta:Μια παράμετρος πραγματικός αριθμός.
-        :param pheromone:Η φεροόνη στις ακμές μεταξύ των κόμβων(πίνακας).
-        :param visibility:Η "ορατότητα" στις ακμές μεταξύ των κόμβων(πίνακας)
-        :param allowed: Οι κόμβοι που επιτέπεται να πάει το μυρμήγκι(Λίστα)
+        :param pheromone:Η φεροόνη στις ακμές μεταξύ των πόλεων(πίνακας).
+        :param visibility:Η "ορατότητα" στις ακμές μεταξύ των πόλεων(πίνακας)
+        :param allowed_towns: Οι πόλεις που επιτέπεται να επισκεφτεί το μυρμήγκι(Λίστα)
         :return: Πιθανότητα πραγματικός αριθμος.
         """
-        # Αν ο επόμενος κόμβος βρίσκεται στη λίστα με τους κόμβους που επιτρέπεται να πάει το μυρμήγκι.
-        if next_node in allowed:
-            # Η φερομόνη στην ακμή απο τον κόμβο i στον j
-            pheromone_i_j = pheromone[located_node][next_node] ** alpha
-            # "ορατότητα" στην ακμή απο τον κόμβο i στον j
-            visibility_i = visibility[located_node][next_node] ** beta
+        # Αν ο επόμενη πόλη βρίσκεται στη λίστα με τις πόλεις που επιτρέπεται να επισκεφτεί το μυρμήγκι.
+        if next_town in allowed_towns:
+            # Η φερομόνη στην ακμή απο την πόλη i στον j
+            pheromone_i_j = pheromone[located_town][next_town] ** alpha
+            # "ορατότητα" στην ακμή απο την πόλη i στον j
+            visibility_i = visibility[located_town][next_town] ** beta
             a = pheromone_i_j * visibility_i
 
-            # Η φερομόνη σε όλες τις ακμές απο τον κόμβο i προς στις πόλεις που επιτρέπεται
-            pheromone_all = pheromone[located_node][allowed] ** alpha
-            # Η ορατόττητα σε όλες τις ακμές απο τον κόμβο i προς στις πόλεις που επιτρέπεται
-            visibility_all = visibility[located_node][allowed] ** beta
+            # Η φερομόνη σε όλες τις ακμές απο την πόλη i προς στις πόλεις που επιτρέπεται
+            pheromone_all = pheromone[located_town][allowed_towns] ** alpha
+            # Η ορατόττητα σε όλες τις ακμές απο την πόλη i προς στις πόλεις που επιτρέπεται
+            visibility_all = visibility[located_town][allowed_towns] ** beta
             b = sum(pheromone_all * visibility_all)
 
             return a / b
 
         else:
             return 0
+
+    @staticmethod
+    def compute_tour_length(ant, a_graph):
+        """
+        Υπολογισμός διαδρομής κάθε μυρμηγκιού.
+        :param ant: Αντικείμενο τύπου Αnt.(Ant)
+        :param a_graph: Γράφος σε μορφη πίνκα.(array)
+        :return: συνολικό μήκος διαδρομής.(integer)
+        """
+        # Άρχικοποίηση
+        length_tour = 0
+        # Υπολογισμος απο την λίστα με τις ακμές το μήκος της συνολικής διαδρομής
+        for i in range(0, len(a_graph)):
+            length_tour += a_graph[ant.get_tour()[i][0]][ant.get_tour()[i][1]]
+        return length_tour
--- AntSystem/Colony.py
+++ AntSystem/Colony.py
@@ -1,4 +1,3 @@
-from AntSystem.ReadData import ReadData
 from AntSystem.Ant import Ant
 import numpy as np
 import random as rand
@@ -9,52 +8,88 @@ class Colony:
     Κλάση AntSystem περίέχει όλες τις λειτουργείες την αποικίας για να βρέθει η συνομότερη διαδρομή στον πρόβλημα TSP
     """
 
-    def __init__(self, dimension, a_graph, number_of_ants):
+    def __init__(self, dimension, a_graph, number_of_ants, alpha, beta, evaporation_rate, number_of_cycles):
         """
         Στον κατασκευαστή γίνεται η αρχικοπίηση των δεδομένων που είναι απαραίτητα για την λειτουργία της αποικίας.
-        - self.dimension: Διάσταση του προβήματος
-        - self.graph: Ο γράφος σε μορφή πίνακα
-        - self.ants: Λίστα με τα μυρμήγκια της αποικίας
-        - self.number_of_ants: Αριθμος μυρμηγκιών
+        - self.dimension: Διάσταση του προβήματος.
+        - self.a_graph: Ο γράφος σε μορφή πίνακα.
+        - self.ants: Λίστα με τα μυρμήγκια της αποικίας.
+        - self.number_of_ants: Αριθμος μυρμηγκιών.
+        - self.alpha: Mia παραμετρος a ελέγχει την σχετική σημασιά της φερομόνης.
+        - self.beta: Mia παραμετρος β ελέγχει την σχετική σημασιά της "ορατότητας".
+        - self.evaporation_rate: Ο ρυθμός εξάτμησης της φερομόνης σε κάθε κύκλο
+        - self.number_of_cycles: Ο αριθμός κύκλων που θα εκλτελέσει η αποικία.
         :param dimension: Διάσταση του προβλήματος.
         :param a_graph: Γράφος σε μόρφη πίνακα.
         """
         self.dimension = dimension
         self.a_graph = a_graph
         self.number_of_ants = number_of_ants
+        self.alpha = alpha
+        self.beta = beta
+        self.evaporation_rate = evaporation_rate
+        self.number_of_cycles = number_of_cycles
         self.ants = list()
 
-    def set_demesion(self, dimension):
+    def set_dimension(self, dimension):
         """
         Ορίζει την διάσταση του προβήματος.
-        :param dimension: Διάσταση πρόβληματος
+        :param dimension: integer
         """
         self.dimension = dimension
 
     def set_graph(self, a_graph):
         """
         Ορίζει το γράφο σε μορφή πίνακα.
-        :param a_graph: Πίνακα
+        :param a_graph: array
         """
         self.a_graph = a_graph
 
     def set_number_of_ants(self, number_of_ants):
         """
         Ορίζει τον αριθμο τον μυρμηγκιών στην αποικία.
-        :param number_of_ants: Ακέραιος αριθμός μυρμηγκιών.
+        :param number_of_ants: integer
         """
+
+    def set_alpha(self, alpha):
+        """
+        Όρίζει την παραμετρος a ελέγχει την σχετική σημασιά της φερομόνης.
+        :param alpha: real
+        """
+        self.alpha = alpha
+
+    def set_beta(self, beta):
+        """
+        Όρίζει την παραμετρος β ελέγχει την σχετική σημασιά της "ορατότητας".
+        :param beta: real
+        """
+        self.beta = beta
+
+    def set_evaporation_rate(self, evaporation_rate):
+        """
+        Ορίζει τον  ρυθμό εξάτμησης της φερομόνης σε κάθε κύκλο
+        :param evaporation_rate: real
+        """
+        self.evaporation_rate = evaporation_rate
+
+    def set_number_of_cycles(self, number_of_cycles):
+        """
+        Ορίζει τον αριθμό των κλυκλων που θα εκτελέσει η αποικία
+        :param number_of_cycles: integer
+        """
+        self.number_of_cycles = number_of_cycles
 
     def set_ants(self, ants):
         """
         Ορίζει την λίστα με τα μυρμήγκια της αποικίας.
-        :param ants: Λίστα με α μυρμήγκια της αποικίας
+        :param ants: list
         """
         self.ants = ants
 
-    def get_demension(self):
+    def get_dimension(self):
         """
         Επιστρέφει την διάσταση του προβήματος(integer).
-        :return: Ενας ακέραιο διάσταση πρόβληματος.
+        :return: integer
         """
         return self.dimension
 
@@ -68,50 +103,51 @@ class Colony:
     def get_number_of_ants(self):
         """
         Επιστρέφει τον αριθμό τον μυρμηγκιών στην αποικία.
-        :return: Ακέραιος αριθμός μυρμηγκιών.
+        :return: integer
         """
         return self.number_of_ants()
+
+    def get_alpha(self):
+        """
+        Επιστρέφει το α μια παράμετρος που ελέγχει την σχετική σημασιά της φερομόνης.
+        :return: real
+        """
+        return self.alpha
+
+    def get_beta(self):
+        """
+        Επιστρέφει το β μια παράμετρος που ελέγχει την σχετική ορατότητα της φερομόνης.
+        :return: real
+        """
+        return self.beta
+
+    def get_evaporation_rate(self):
+        """
+        Επιστρέφει το ρυθμό εξάτμισης της φερομόνης της αποικίας σε κάθε κύκλο
+        :return: real
+        """
+        return self.evaporation_rate
+
+    def get_number_of_cycles(self):
+        """
+        Επιστρέφει τον αρθμό των κύκλων που θα εκτελέσει η αποικία.
+        :return: integer
+        """
+        return self.number_of_cycles
 
     def get_ants(self):
         """
         Επιστρέφει την λίστα με τα μυρμήγκια της αποικίας.
-        :return: Λίστα
+        :return: list
         """
         return self.ants
-
-    def run(self):
-        """
-        Τρέχει την αποικία.
-        :return:
-        """
-        # Initialize
-        cycles = 0
-        initial_pheromone = Colony.initialize_pheromone(self.dimension, 1)
-        pheromone = initial_pheromone
-        delta_pheromone = np.zeros((self.dimension, self.dimension))
-        print(self.a_graph)
-        visibility = colony.set_visibility(self.a_graph)
-
-        ants = Colony.initialize_ants(self.ants, self.number_of_ants, self.dimension)
-        print("============================Initialize========================================")
-        for k in range(0, self.number_of_ants):
-            print("ant id:", ants[k].get_ant_id())
-            print("starting node:", ants[k].get_starting_node())
-            print("allowed nodes:", ants[k].get_allowed_nodes())
-
-        print("==========================TOURS=========================================")
-        for k in range(0, self.number_of_ants):
-            print("ant", k)
-            ant = ants[k]
-            tour = colony.built_tour(ant, 0.7, 0.7, pheromone, visibility, self.dimension)
-            print(tour)
 
     @staticmethod
     def set_visibility(a_graph):
         """
         Ορίζει την "ορατότητα" του γράφου.
-        :param a_graph: Πίνακας
-        :return: Πίνακα
+        :param a_graph: array
+        :return: array
         """
         visibility = 1 / a_graph
         return visibility
@@ -121,14 +157,14 @@ class Colony:
         """
         Αρχικοποίηση της φερομόνης.
         :param dimension: Η διάσταση τοτ προβλήματος.
-        :param initial_value: Αρχική τιμή που θα οριστεί.
-        :return: Επιστροφή πίνακα με τις αρχικές τιμές την φερομόνης.
+        :param initial_value: Αρχική τιμή που θα οριστεί(integer).
+        :return: Επιστροφή πίνακα με τις αρχικές τιμές την φερομόνης(array).
         """
         initial_pheromone = np.zeros((dimension, dimension))
         for row in range(0, dimension):
             for column in range(0, dimension):
                 if row == column:
-                    initial_pheromone[row][column] = 1
+                    initial_pheromone[row][column] = 0
                 else:
                     initial_pheromone[row][column] = initial_value
         return initial_pheromone
@@ -139,21 +175,18 @@ class Colony:
         Αρχικοποίηση μυρμήγκιων σε τυχαίες αρχικές θέσεις.
         :param ants: Λίστα άδεια
         :param number_of_ants: Ο συνολικος αριθμος των μυρμηγκιών στην αποίκια
-        :param dimension: ακέραιος με την διάσταση του προβλήματος.
-        :return: Λίστα αντικειμέων τύπου Αnt με τα μυμρήγκια της αποικίας αρχικοποιημένα στις αρχικές θέσεις.
+        :param dimension: ακέραιος με την διάσταση του προβλήματος(integer).
+        :return: Λίστα αντικειμέων τύπου Αnt με τα μυμρήγκια της αποικίας αρχικοποιημένα στις αρχικές θέσεις(list).
         """
         for ant_id in range(0, number_of_ants):
             # Τυχαίος αριθμός είναι  dimension -1 γιατό ξεκινάει απο το 0
-            starting_node = rand.randint(0, dimension - 1)
-            # Δημιουργία ενός αντικειμένου τυπου Αnt ορίζω το id του μυμρμηγκιού, και την τυχαία αρχική θέση
-            ants.append(Ant(ant_id, starting_node))
+            starting_town = rand.randint(0, dimension - 1)
+            # Δημιουργία ενός αντικειμένου τυπου Αnt ορίζω το id του μυμρμηγκιού, και την τυχαία αρχική πόλη
+            ants.append(Ant(ant_id, starting_town))
             # Για κάθε μυρμήγκι ορίζω τους κόμβους που επιτρέπεται να πάει
-            ants[ant_id].set_allowed_nodes(Ant.initialize_allowed_nodes(dimension))
+            ants[ant_id].set_allowed_towns(Ant.initialize_allowed_towns(dimension))
             # Αφαιρώ απο την λίστα με τους επιτρεπτόμενους κόμβους τον αρχικο κόμβο
-            ants[ant_id].get_allowed_nodes().remove(ants[ant_id].get_starting_node())
-
-            ants[ant_id].set_tour(ants[ant_id].get_starting_node())
-
+            ants[ant_id].get_allowed_towns().remove(ants[ant_id].get_starting_town())
         return ants
 
     @staticmethod
@@ -170,9 +203,10 @@ class Colony:
         """
         # Μέχρι να αδειάσει η λίστα με τις πόλεις που επιτεπεται να πάει το μυρμήγκι
         # είναι το σύνολο των πόλεων -1 γιάτι ήδη έχουμε αφαιρέσει απο την λίστα την αρχική πόλη.
+        global next_town
         for iteration in range(0, dimension - 1):
-            located_town = ant.get_located_node()
-            allowed_towns = ant.get_allowed_nodes()
+            located_town = ant.get_located_town()
+            allowed_towns = ant.get_allowed_towns()
             max_probability = 0
             # Για κάθε τιμή της λίστας
             for index in range(0, len(allowed_towns)):
@@ -183,26 +217,119 @@ class Colony:
                                                          allowed_towns)
                 if probability > max_probability:
                     max_probability = probability
-                    next_node = allowed_towns[index]
-            # Τρέχον πόλη γίνεται η πόλη με την μεγαλύτερη πιθανότητα.
-            ant.set_located_node(next_node)
-            # Την προσθέτει στην λίστα με την διαδρομή.
-            ant.set_tour(next_node)
+                    next_town = allowed_towns[index]
+
+            # Προθέτει στην λίστα την πόλη με την μεγαλύτερη πιθανότητα.
+            ant.set_tour((ant.located_town, next_town))
+            # Γίνεται τρέχον πόλη η πόλη με την μεγαλύτερη πιθανότητα.
+            ant.set_located_town(next_town)
             # Την αφαιρεί απο την λίστα με τις επιτρεπόμενες πόλεις
-            ant.get_allowed_nodes().remove(ant.get_located_node())
+            ant.get_allowed_towns().remove(ant.get_located_town())
+        # Προσθέτει στην λίστα με την διαδρομή την αρχική πόλη
+        ant.set_tour((ant.get_located_town(), ant.get_starting_town()))
+        # Γίνεται τρέχον πόλη η αρχική.
+        ant.set_located_town(ant.get_starting_town())
         return ant.get_tour()
 
+    @staticmethod
+    def quantity_per_unit_of_length_of_pheromone(ants, tour_length, quantity):
+        """
+        Υπολογισμός της ποσότητας φερομόνης ανα μονάδα συνολικού μήκους διαδρομης
+        κάθε ακμής που την  επισκέφτηκε κάθε μηρμήγκι.
 
-# read_data = ReadData("../data/gr17.xml")
-# data = read_data.get_tree()
-# name = read_data.get_name_of_file()
-# data = ReadData.read_file(name)
-data = np.array([[0, 3, 6, 2, 3], [3, 0, 5, 2, 3], [6, 5, 0, 6, 4], [2, 2, 6, 0, 6], [3, 3, 4, 6, 0]])
-# print(data)
-# dimension = ReadData.get_dimension(data)
-# print(len(data))
-# graph = ReadData.create_graph(data, dimension)
-# print(graph)
+        :param quantity: Πίνακας με την ποσότητα φερομόνης...σε κάθε ακμη
+        :param ants: Λίστα με τα μυρμήγκια που βρίσκονται στης αποκία.
+        :param tour_length:Συνολικό μήκος διαδρομής
+        :return: ποσότητας φερομόνης ανα μονάδα συνολικού μήκους διαδρομης σε κάθε ακμή
+        """
+        for i in range(0, len(quantity)):
+            for j in range(0, len(quantity)):
+                for k in range(len(ants)):
+                    if (i, j) in ants[k].get_tour():
+                        d = 1 / tour_length[k]
+                    else:
+                        d = 0
+                    quantity[i][j] += d
+        return quantity
 
-colony = Colony(len(data), data, 5)
-colony.run()
+    @staticmethod
+    def update_pheromone(pheromone, quantity, evaporation_rate):
+        """
+        Ενημέρωση φερομόνης στις ακμές του γράφου
+
+        :param evaporation_rate: Ρυθμός εξάτμισης φερομόνης(real)
+        :param pheromone: Πινακας με τις τιμές της φερομόνης πάνω στις ακμές του γράφου(array)
+        :param quantity: Πινακας με τις τιμές της φερομόνης ανα μονάδα συνολικού μήκους διαδρομης
+            πάνω στις ακμές του γράφου(array)
+        :return:Πίανκας με την ενημερωμένη φερομόνη(array).
+        """
+        for i in range(0, len(pheromone)):
+            for j in range(0, len(pheromone)):
+                pheromone[i][j] = evaporation_rate * pheromone[i][j] + quantity[i][j]
+        return pheromone
+
+    def run(self):
+        """
+        Τρέχει την αποικία.
+        """
+        # Αρχικοποιήση
+        # Αρχικοποιώ την συντομότερη διαδρομή άπειρο.
+        min_length_all_time = np.inf
+        min_tour = 0
+        # Αρχικοποήση μίας λίστας που θα περέχει το μήκως των διαδρμών των μυρμήγκιων.
+        length_tours = list()
+        # Αρχικοποιήση της αρχικής φερομόνης στις ακμές ματαξύ των πόλεων.
+        initial_pheromone = Colony.initialize_pheromone(self.dimension, 1)
+        pheromone = initial_pheromone
+        # Αρχικοποίηση ενός πινακα n * n διαστάσεων που θα περέχει μετα τον υπολογισμό την ποσότητας φερομόνης ανα
+        # μονάδα συνολικού μήκους διαδρομης κάθε ακμής που την  επισκέφτηκε κάθε μηρμήγκι.
+        quantity = np.zeros((self.dimension, self.dimension))
+        # Αρχικοποίηση ενός πίνακα n * n που θα περιέχει μετα τον υπολογισμό θα περιέχει την "ορατότητα" στις ακμές
+        # μεταξύ των πόλεων.
+        visibility = Colony.set_visibility(self.a_graph)
+        # Αρχικοποίση μυρμήγκιών στην αποικία.
+        ants = Colony.initialize_ants(self.ants, self.number_of_ants, self.dimension)
+        print("============================Initialize========================================")
+        # for k in range(0, self.number_of_ants):
+        #     print("ant id:", ants[k].get_ant_id())
+        #     print("starting node:", ants[k].get_starting_town())
+        #     print("allowed nodes:", ants[k].get_allowed_towns())
+
+        # print("==========================TOURS=================================================")
+        # Για κάθε κύκλο.
+        nc = 0
+        while nc < self.number_of_cycles:
+            # Για κάθε μυρμήγκι.
+            for k in range(0, self.number_of_ants):
+                # print("ant", k)
+                ant = ants[k]
+                # Δημηουργία διαδρομής του μυρμηγκιού
+                tours = Colony.built_tour(ant, self.alpha, self.beta, pheromone, visibility, self.dimension)
+                # print(tours)
+                # Υπολογισμός κλοστους της διαδρομής και εισαγωγή στην λίστα
+                length_tours.append(Ant.compute_tour_length(ant, self.a_graph))
+                # print("length:", length_tours)
+            # Υπολογισμός ποσότητας φερομόνης ανα μονάδα συνολικού μήκους διαδρομης κάθε ακμής
+            quantity = Colony.quantity_per_unit_of_length_of_pheromone(self.ants, length_tours, quantity)
+            # Ενημέρωση φερομόνης.
+            pheromone = Colony.update_pheromone(pheromone, quantity, self.evaporation_rate)
+
+            min_length = min(length_tours)
+            if min_length < min_length_all_time:
+                min_length_all_time = min_length
+                min_tour = ants[length_tours.index(min(length_tours))].get_tour()
+                print(min_length_all_time)
+                print(min_tour)
+
+            for k in range(0, self.number_of_ants):
+                ants[k].set_allowed_towns(Ant.initialize_allowed_towns(self.dimension))
+                ants[k].get_tour().clear()
+                length_tours.clear()
+                ants[k].set_located_town(ants[k].get_starting_town())
+                ants[k].get_allowed_towns().remove(ants[k].get_starting_town())
+            nc += 1
+        print("Parameters:")
+        print("alpha", self.alpha)
+        print("beta", self.beta)
+        print("evaporation_rate", self.evaporation_rate)
+        print("Minimum length:", min_length_all_time, ":", min_tour)
